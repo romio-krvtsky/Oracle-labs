13851b75087aa5604e88f1fb770212e083fe84ca
romio-krvtsky <romik.krivetskiy@gmai.com>
2023-03-21 01:32:44 +0300

    created procedure compare_schema

---
+++ Lab3/Lab3.sql
@@ -0,0 +1,11 @@
+create or replace PROCEDURE COMPARE_SCHEMA(dev_schema_name VARCHAR2, prod_schema_name VARCHAR2)
+    IS
+    diff_counter1 NUMBER;
+    diff_counter2 NUMBER;
+    code          VARCHAR2(100);
+BEGIN
+
+
+
+END;
+
